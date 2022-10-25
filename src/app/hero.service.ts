c946b0e1ebbe71b502ee918115fdadf62c2248a9
Jakub Tuzar <jtuzar@csas.cz>
2022-10-25 10:09:57 +0200

    Added message service for logging purposes.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
-
-  constructor() { }
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    return heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
